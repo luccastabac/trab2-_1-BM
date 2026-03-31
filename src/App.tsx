daa83b0203f89fbc79f63fb5ccac47adc795b241
luccastabac <luccastabac@gmail.com>
2026-03-30 21:25:32 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -39,8 +39,7 @@ function App() {
   const carros: Carro[] = [
     {
       nome: "Bugatti Chiron",
-      imagem:
-        "https://www.velhochiconews.com/hf-conteudo/uploads/posts/2024/07/1888_ad0b3997a4a6d2eaf68d6e4882aedead.jpg",
+      imagem: "https://www.velhochiconews.com/hf-conteudo/uploads/posts/2024/07/1888_ad0b3997a4a6d2eaf68d6e4882aedead.jpg",
       velocidade: 420,
       potencia: 1500,
       zeroCem: 2.4,
@@ -49,8 +48,7 @@ function App() {
     },
     {
       nome: "Koenigsegg Jesko",
-      imagem:
-        "https://www.thesupercarblog.com/wp-content/uploads/2023/10/Koenigsegg-Jesko-Attack-Plus-Green-spec-3.jpg",
+      imagem: "https://www.thesupercarblog.com/wp-content/uploads/2023/10/Koenigsegg-Jesko-Attack-Plus-Green-spec-3.jpg",
       velocidade: 480,
       potencia: 1600,
       zeroCem: 2.5,
@@ -59,8 +57,7 @@ function App() {
     },
     {
       nome: "Hennessey Venom F5",
-      imagem:
-        "https://s2-autoesporte.glbimg.com/Js1qPEf5VtrlGK4nVKmzgx283pc=/0x0:1500x981/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2023/Z/U/ASKDKOSZKKSWvSX2HdOQ/hennessey-venom-f5-revolution-roadster-high-1.jpg",
+      imagem: "https://s2-autoesporte.glbimg.com/Js1qPEf5VtrlGK4nVKmzgx283pc=/0x0:1500x981/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2023/Z/U/ASKDKOSZKKSWvSX2HdOQ/hennessey-venom-f5-revolution-roadster-high-1.jpg",
       velocidade: 500,
       potencia: 1817,
       zeroCem: 2.6,
@@ -69,8 +66,7 @@ function App() {
     },
     {
       nome: "SSC Tuatara",
-      imagem:
-        "https://hips.hearstapps.com/mtg-prod/65c41ca2ecd098000862804e/ssc-na-tuatara-36.jpg",
+      imagem: "https://hips.hearstapps.com/mtg-prod/65c41ca2ecd098000862804e/ssc-na-tuatara-36.jpg",
       velocidade: 455,
       potencia: 1750,
       zeroCem: 2.5,
@@ -88,8 +84,7 @@ function App() {
     },
     {
       nome: "Ferrari SF90 Stradale",
-      imagem:
-        "https://upload.wikimedia.org/wikipedia/commons/1/1e/Ferrari_SF90_Stradale.jpg",
+      imagem: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Ferrari_SF90_Stradale.jpg",
       velocidade: 340,
       potencia: 1000,
       zeroCem: 2.5,
@@ -98,8 +93,7 @@ function App() {
     },
     {
       nome: "Porsche 911 Turbo S",
-      imagem:
-        "https://cdn.motor1.com/images/mgl/Vznj6y/s3/brabus-900-rocket-r.webp",
+      imagem: "https://cdn.motor1.com/images/mgl/Vznj6y/s3/brabus-900-rocket-r.webp",
       velocidade: 330,
       potencia: 650,
       zeroCem: 2.7,
@@ -108,8 +102,7 @@ function App() {
     },
     {
       nome: "McLaren P1",
-      imagem:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/McLaren_P1_GTR_at_Grand_Basel_2018_03.jpg/1280px-McLaren_P1_GTR_at_Grand_Basel_2018_03.jpg",
+      imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/McLaren_P1_GTR_at_Grand_Basel_2018_03.jpg/1280px-McLaren_P1_GTR_at_Grand_Basel_2018_03.jpg",
       velocidade: 350,
       potencia: 916,
       zeroCem: 2.8,
@@ -118,8 +111,7 @@ function App() {
     },
     {
       nome: "Aston Martin Valkyrie",
-      imagem:
-        "https://www.topgear.com/sites/default/files/brightcove/videos/images/posters/1546304439_5844286552001_5844282300001-vs.jpg",
+      imagem: "https://www.topgear.com/sites/default/files/brightcove/videos/images/posters/1546304439_5844286552001_5844282300001-vs.jpg",
       velocidade: 402,
       potencia: 1160,
       zeroCem: 2.5,
@@ -128,8 +120,7 @@ function App() {
     },
     {
       nome: "Pagani Huayra",
-      imagem:
-        "https://cdn.rmsothebys.com/3/e/7/6/3/e/3e763ed063de7afbb4a632d300174dc235879abb.webp",
+      imagem: "https://cdn.rmsothebys.com/3/e/7/6/3/e/3e763ed063de7afbb4a632d300174dc235879abb.webp",
       velocidade: 383,
       potencia: 730,
       zeroCem: 3.0,
@@ -175,10 +166,6 @@ function App() {
         <h1>React Trunfo</h1>
       </div>
 
-      <div className="botoes">
-        <button onClick={sortearCartas}>Sortear Cartas</button>
-      </div>
-
       <h2 className="subtitulo">Duelo de Cartas</h2>
 
       <div className="arena">
@@ -188,6 +175,10 @@ function App() {
       </div>
 
       {resultado && <h3 className="resultado">{resultado}</h3>}
+
+      <div className="botoes">
+        <button onClick={sortearCartas}>Sortear Cartas</button>
+      </div>
 
       <h2 className="subtitulo">Todas as Cartas</h2>
 
